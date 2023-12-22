bee09aa360a24fe6b048c4433012dcc66c53be3f
Priyanshu2746 <87070066+Priyanshu2746@users.noreply.github.com>
2023-12-22 11:33:27 +0530

    Create Rectangle Pattern.java

---
+++ simple patterns/Rectangle Pattern.java
@@ -0,0 +1,13 @@
+public class java_patterns {
+    public static void main(String args[]){
+        /* Square
+        int n=3;
+        int m=2;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
